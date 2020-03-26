6aad6ebd8538e371b3466507800ab1ed76c9dcd5
Leonardo Catelan <leocatelan@gmail.com>
2020-03-26 04:57:01 -0300

    Start page commands created

---
+++ UiTest/Pages/DemoCsStartPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationTest.UiTest.Pages
+{
+    public class DemoCsStartPage:Page
+    {
+        IWebElement searchBox;
+        IWebElement magnify;
+        IWebElement searchResults;
+        IWebElement adidasTShirt;
+        private By searchboxSelector = By.Id("search_input");
+        private By magnifySelector = By.CssSelector("[title='Search']");
+        private By resultsSelector = By.ClassName("ty-mainbox-title");
+        private By adidasTShirtSelector = By.Id("det_img_11");
+
+        public void PerformSearch(string text)
+        {
+            searchBox = wait.Until(ExpectedConditions.ElementToBeClickable(searchboxSelector));
+            magnify = wait.Until(ExpectedConditions.ElementToBeClickable(magnifySelector));
+            searchBox.SendKeys(text);
+            magnify.Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(resultsSelector));
+            adidasTShirt = wait.Until(ExpectedConditions.ElementToBeClickable(adidasTShirtSelector));
+            adidasTShirt.Click();
+        }
+        
+        public DemoCsStartPage()
+        {
+            url = "http://demo.cs-cart.com/";
+        }
+    }
+}
---
+++ UiTest/UiTests.cs
@@ -0,0 +1,31 @@
+﻿using AutomationTest.Core;
+using AutomationTest.UiTest.Pages;
+using NUnit.Framework;
+
+
+namespace AutomationTest.UiTest
+{
+    [TestFixture]
+    public class UiTests
+    {
+        DemoCsStartPage startPage;
+        [SetUp]
+        public void BaseSetUp()
+        {
+            WebDriver.StartDriver();
+            startPage = new DemoCsStartPage();
+        }
+        [TearDown]
+        public void BaseTearDown()
+        {
+            WebDriver.CloseDriver();
+        }
+        [Test]
+        public void BasicNavigation()
+        {
+            startPage.GoToUrl();
+            startPage.PerformSearch("T-Shirt");
+
+        }
+    }
+}
